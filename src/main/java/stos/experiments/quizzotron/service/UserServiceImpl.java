a1c2e45994a9a34c3df26a04f1e8683ddf8cc6ae
Stuart Osborne <stuart.osborne@infinera.com>
2020-03-28 15:02:41 +0100

    Remove temporary service implementation

--- src/main/java/stos/experiments/quizzotron/service/UserServiceImpl.java
+++ src/main/java/stos/experiments/quizzotron/service/UserServiceImpl.java
@@ -3,30 +3,23 @@ package stos.experiments.quizzotron.service;
 import org.springframework.stereotype.Component;
 import stos.experiments.quizzotron.api.ApiUser;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class UserServiceImpl implements UserService {
 
-  private List<ApiUser> registeredUsers;
-  private static Long idCtr;
-
   public UserServiceImpl() {
-    this.registeredUsers = new ArrayList<>();
-    idCtr = 0L;
+
   }
 
   @Override
   public List<ApiUser> getAllRegisteredUsers() {
-    return registeredUsers;
+    return Collections.emptyList();
   }
 
   @Override
   public ApiUser registerUser(ApiUser user) {
-    user.setId(idCtr);
-    registeredUsers.add(user);
-    idCtr++;
-    return user;
+    return null;
   }
 }
