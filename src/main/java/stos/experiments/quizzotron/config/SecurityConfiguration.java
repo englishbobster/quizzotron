ce107a08a921dcb6f07644b4523267195c558a2d
Stuart Osborne <stuart.osborne@infinera.com>
2020-04-04 15:01:43 +0200

    Configured spring security properly.

--- src/main/java/stos/experiments/quizzotron/config/SecurityConfiguration.java
+++ src/main/java/stos/experiments/quizzotron/config/SecurityConfiguration.java
@@ -2,11 +2,25 @@ package stos.experiments.quizzotron.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/**").permitAll()
+        .antMatchers(HttpMethod.GET, "/**").permitAll()
+        .and()
+        .csrf().disable();
+  }
 
   @Bean
   PasswordEncoder getPasswordEncoder() {
